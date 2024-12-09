17bc9abd9906ade2391470c1b326736332d1b247
TJ Gillis <bardic.knowledge@gmail.com>
2024-12-09 08:13:55 -0400

    arud

--- .dagger/main.go
+++ .dagger/main.go
@@ -4,12 +4,30 @@ import (
 	"context"
 	"dagger/env/internal/dagger"
 	"fmt"
+	"os"
 	"strings"
 )
 
 type Env struct{}
 
-func (m *Env) Load(ctx context.Context, f *dagger.File, c *dagger.Container) (*dagger.Container, error) {
+func (m *Env) Host(ctx context.Context, f *dagger.File) {
+	envs, err := f.Contents(ctx)
+
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+	}
+
+	envPair := strings.Split(envs, "\n")
+
+	for _, v := range envPair {
+		envVals := strings.SplitN(v, "=", 2)
+		os.Setenv(envVals[0], envVals[1])
+	}
+
+	return
+}
+
+func (m *Env) Container(ctx context.Context, f *dagger.File, c *dagger.Container) (*dagger.Container, error) {
 	envs, err := f.Contents(ctx)
 
 	if err != nil {
